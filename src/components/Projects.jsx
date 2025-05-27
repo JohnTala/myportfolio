af7c143ed465334a6c2d441de3e2f7a8ea93e12a
buy-st <stalatala@byupathway.edu>
2025-05-27 17:55:32 +0200

    3rd commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,9 +6,8 @@ export default function Projects() {
       <div className="max-w-4xl mx-auto">
         <h3 className="text-3xl font-bold mb-6">Projects</h3>
         <ul className="space-y-4">
-          <li className="bg-gray-100 p-4 rounded shadow"><a href="https://JohnTala.github.io/wdd131">Project 1 - Portfolio Website</a></li>
-          <li className="bg-gray-100 p-4 rounded shadow"><a href="http://JohnTala.github.io/myfoodiewebsite">Project 2 - zeus website</a></li>
-          <li className="bg-gray-100 p-4 rounded shadow">Project 2 - React Todo App</li>
+          <li className="bg-gray-100 p-4 rounded shadow"><a href="https://JohnTala.github.io/myfoodiewebsite">Project 1 - Zeus Restaurant</a></li>
+          <li className="bg-gray-100 p-4 rounded shadow"><a href="http://JohnTala.github.io/A_Todo_list">Project 2 - A Todo_List</a></li>
           <li className="bg-gray-100 p-4 rounded shadow">Project 3 - Weather App</li>
         </ul>
       </div>
